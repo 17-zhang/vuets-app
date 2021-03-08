cc930a3987e530efd677a010c6beda7d89924828
zyq-Tomato <1562070422@qq.com>
2021-03-08 23:42:31 +0800

    project init

---
+++ src/router.ts
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import VueRouter, { RouteConfig } from 'vue-router'
+
+Vue.use(VueRouter)
+
+const routes: Array<RouteConfig> = [
+  {
+    path: '/home',
+    name: 'home',
+    component: () => import("@/views/Home.vue")
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import("@/views/Login/Login.vue")
+  },
+  {
+    path: '/password',
+    name: 'password',
+    component: () => import("@/views/Login/Password.vue")
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
